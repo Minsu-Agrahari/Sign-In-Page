179c4a5cdf4638fb820b9e486732d8d3a33a8565
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-05-13 22:22:29 +0530

    format: apply consistent code formatting and add Prettier configuration files

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, View , ScrollView} from "react-native";
+import { SafeAreaView, View, ScrollView } from "react-native";
 
 import AuthHeader from "../components/AuthHeader";
 import InputField from "../components/InputField";
@@ -7,46 +7,46 @@ import SocialButtons from "../components/SocialButtons";
 import FooterLinks from "../components/FooterLinks";
 
 export default function HomeScreen() {
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: "#F7F7F7",
-      }}
-    >
-      <ScrollView
-        style={{
-          flex: 1,
-          paddingHorizontal: 24,
-          paddingTop: 70,
-        }}
-      >
-        <AuthHeader
-          title="Sign In"
-          subtitle="Let's experience the joy of telecare AI."
-        />
+    return (
+        <SafeAreaView
+            style={{
+                flex: 1,
+                backgroundColor: "#F7F7F7",
+            }}
+        >
+            <ScrollView
+                style={{
+                    flex: 1,
+                    paddingHorizontal: 24,
+                    paddingTop: 70,
+                }}
+            >
+                <AuthHeader
+                    title="Sign In"
+                    subtitle="Let's experience the joy of telecare AI."
+                />
 
-        <InputField
-          label="Email Address"
-          placeholder="Enter your email..."
-        />
+                <InputField
+                    label="Email Address"
+                    placeholder="Enter your email..."
+                />
 
-        <InputField
-          label="Password"
-          placeholder="Enter your password..."
-          secureTextEntry={true}
-        />
+                <InputField
+                    label="Password"
+                    placeholder="Enter your password..."
+                    secureTextEntry={true}
+                />
 
-        <PrimaryButton title="Sign In" />
+                <PrimaryButton title="Sign In" />
 
-        <SocialButtons />
+                <SocialButtons />
 
-        <FooterLinks
-          text="Don't have an account?"
-          actionText="Sign Up"
-          route="/signup"
-        />
-      </ScrollView>
-    </SafeAreaView>
-  );
+                <FooterLinks
+                    text="Don't have an account?"
+                    actionText="Sign Up"
+                    route="/signup"
+                />
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
